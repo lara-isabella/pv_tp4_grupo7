0e715b3a8aed0a820f0651b85c0b93f3ab1f1de0
Yamila Quipildor <quipildor.yam@gmail.com>
2025-05-20 00:06:38 -0300

    Actualización barraBusqueda.jsx - Yamila Quipildor

--- src/assets/components/barraBusqueda.jsx
+++ src/assets/components/barraBusqueda.jsx
@@ -1,20 +1,32 @@
-//Barra para busqueda de productos
-import React, { useCallback } from "react";
+//Barra para la búsqueda de productos
+import { useState, useCallback } from "react";
 
-const BarraBusqueda = ({ alBuscar }) => {
-  const manejarCambio = useCallback((e) => {
-    alBuscar(e.target.value);
-  }, [alBuscar]);
+function BarraBusqueda({ alBuscar }) {
+  const [terminoLocal, setTerminoLocal] = useState("");
+
+  const manejarCambio = (e) => {
+    setTerminoLocal(e.target.value);
+  };
+
+  const manejarBusqueda = useCallback(
+    (e) => {
+      e.preventDefault(); // Evita que se recargue la página al enviar el formulario
+      alBuscar(terminoLocal);
+    },
+    [terminoLocal, alBuscar]
+  );
 
   return (
-    <div>
+    <form onSubmit={manejarBusqueda}>
       <input
         type="text"
-        placeholder="Busca por nombre o ID"
+        placeholder="Busca por nombre o ID :)"
+        value={terminoLocal}
         onChange={manejarCambio}
       />
-    </div>
+      <button type="submit">Buscar</button>
+    </form>
   );
-};
+}
 
 export default BarraBusqueda;
